7b5c651b9dbab277d11d5cb5416c3058a9d8761c
jeremyrdavis <jeremy.davis@redhat.com>
2021-06-28 21:46:28 -0400

    passing SSE test

--- src/test/io/quarkuscoffeeshop/web/infrastructure/DashboardResourceTest.java
+++ src/test/io/quarkuscoffeeshop/web/infrastructure/DashboardResourceTest.java
@@ -1,11 +1,21 @@
 package io.quarkuscoffeeshop.web.infrastructure;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkuscoffeeshop.domain.Item;
+import io.quarkuscoffeeshop.domain.OrderStatus;
+import io.quarkuscoffeeshop.web.domain.DashboardUpdate;
 import io.quarkuscoffeeshop.web.testing.KafkaTestResource;
+import io.smallrye.reactive.messaging.connectors.InMemoryConnector;
+import io.smallrye.reactive.messaging.connectors.InMemorySource;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -13,31 +23,54 @@ import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import static io.restassured.RestAssured.when;
+import static org.junit.jupiter.api.Assertions.*;
 
-@QuarkusTest @QuarkusTestResource(KafkaTestResource.class) @TestHTTPEndpoint(DashboardResource.class)
+@QuarkusTest
+@QuarkusTestResource(KafkaTestResource.class)
+@TestHTTPEndpoint(DashboardResource.class)
 public class DashboardResourceTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DashboardResourceTest.class);
+
+    private static final String expectedPayload = "data: {\"orderId\":\"82124c69-a108-4ccc-9ac4-64566e389178\",\"itemId\":\"f84cb5e2-a3fd-43af-8df8-b5d74b133115\",\"name\":\"Scotty\",\"item\":\"COFFEE_WITH_ROOM\",\"status\":\"IN_QUEUE\",\"madeBy\":\"\"}";
+
+    @Inject
+    @Any
+    InMemoryConnector connector;
 
     @Test
     public void testStreaming() {
 
         try {
+            DashboardUpdate dashboardUpdate = new DashboardUpdate(
+                    "82124c69-a108-4ccc-9ac4-64566e389178",
+                    "f84cb5e2-a3fd-43af-8df8-b5d74b133115",
+                    "Scotty",
+                    Item.COFFEE_WITH_ROOM,
+                    OrderStatus.IN_QUEUE,
+                    Optional.empty()
+            );
+            InMemorySource<String> ordersIn = connector.source("web-updates");
+            ordersIn.send(JsonUtil.toJson(dashboardUpdate));
+            await().atLeast(2, TimeUnit.SECONDS);
+
             URI uri = new URI("http://localhost:8080/dashboard/stream");
             HttpClient httpClient = HttpClient.newHttpClient();
             HttpRequest httpRequest = HttpRequest.newBuilder(uri).GET().build();
             Stream<String> lines = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofLines()).body();
-            String result = lines.findFirst().get();
-            assertNull(result, "Result should not be null");
-//            when().get()
-//                    .then()
-//                    .statusCode(200)
-//                    .body(is("hello"));
+
+            Optional<String> payload = lines.filter(l -> l.length() >= 1).findFirst();
+            assertTrue(payload.isPresent());
+            assertEquals(expectedPayload, payload.get());
         } catch (URISyntaxException e) {
             assertNull(e, "Exception thrown");
         } catch (IOException e) {
